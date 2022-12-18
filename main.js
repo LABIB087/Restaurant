5be141571158e947f49cb1912aaa80c389b39743
Labib-Ahammed <101618935+LABIB087@users.noreply.github.com>
2022-12-18 17:02:25 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+var bar = document.getElementById("bars");
 
+var menu = document.getElementById("menu");
+
+bar.addEventListener('click', () => {
+    if (menu.className === 'hidden') {
+        menu.classList.remove('hidden');
+    }
+    else {
+        menu.classList.add('hidden');
+    }
+});
+
+console.log("Labib Ahammed");
+console.log("My website link:: labib007.unaux.com ");
